22dc5e169908f11b89faecc471fd6f8d6b7f9d9c
Alexey Zatsepin <alexzatsepin7@gmail.com>
2016-03-12 17:24:29 +0200

    add new db

--- .openshift/action_hooks/deploy.sh
+++ .openshift/action_hooks/deploy.sh
@@ -3,6 +3,7 @@
 source $OPENSHIFT_HOMEDIR/python/virtenv/bin/activate
 cd $OPENSHIFT_REPO_DIR
 echo "Executing 'python manage.py migrate'"
+python manage.py makemigrations
 python manage.py migrate
 echo "Executing 'python manage.py collectstatic --noinput'"
 python manage.py collectstatic --noinput
