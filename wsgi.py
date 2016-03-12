a9fb14ac3c969659553a6d729c5a6a1df213582d
Alexey Zatsepin <alexzatsepin7@gmail.com>
2016-03-12 14:22:36 +0200

    mm

--- wsgi.py
+++ wsgi.py
@@ -11,7 +11,7 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-from mytest.mytest.wsgi import application
+from mytest.wsgi import application
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
