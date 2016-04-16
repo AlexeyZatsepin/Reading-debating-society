98b9a213d5fededd52fd40c727f27a31315e688d
Alexey Zatsepin <alexzatsepin7@gmail.com>
2016-04-16 15:05:18 +0300

    a

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.sitemaps import GenericSitemap
 from django.http import HttpResponse
 from django.contrib.sitemaps.views import sitemap
-
 from .sitemap import sitemaps
 from . import settings
 from django.conf.urls.static import static
